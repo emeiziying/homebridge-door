7ff0e3e6b783a2f6b1cfac74ab130c4618f8931d
emeiziying <dzq1007650105@gmail.com>
2020-07-01 15:14:47 +0800

    update

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -62,10 +62,6 @@ export class DoorAccessory {
       this.accessory.getService(this.platform.Service.Switch) ||
       this.accessory.addService(this.platform.Service.Switch);
 
-    // To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-    // when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-    // this.accessory.getService('NAME') ?? this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE');
-
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
     this.service.setCharacteristic(
@@ -73,41 +69,11 @@ export class DoorAccessory {
       accessory.context.device.name,
     );
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
     // register handlers for the On/Off Characteristic
     this.service
       .getCharacteristic(this.platform.Characteristic.On)
       .on('set', this.setOn.bind(this)) // SET - bind to the `setOn` method below
       .on('get', this.getOn.bind(this)); // GET - bind to the `getOn` method below
-
-    // register handlers for the Brightness Characteristic
-    // this.service
-    //   .getCharacteristic(this.platform.Characteristic.Brightness)
-    //   .on('set', this.setBrightness.bind(this)); // SET - bind to the 'setBrightness` method below
-
-    // EXAMPLE ONLY
-    // Example showing how to update the state of a Characteristic asynchronously instead
-    // of using the `on('get')` handlers.
-    //
-    // Here we change update the brightness to a random value every 5 seconds using
-    // the `updateCharacteristic` method.
-    // setInterval(() => {
-    //   // assign the current brightness a random value between 0 and 100
-    //   const currentBrightness = Math.floor(Math.random() * 100);
-
-    //   // push the new value to HomeKit
-    //   this.service.updateCharacteristic(
-    //     this.platform.Characteristic.Brightness,
-    //     currentBrightness,
-    //   );
-
-    //   this.platform.log.debug(
-    //     'Pushed updated current Brightness state to HomeKit:',
-    //     currentBrightness,
-    //   );
-    // }, 10000);
   }
 
   /**
@@ -146,28 +112,11 @@ export class DoorAccessory {
     callback(null);
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   * 
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   * 
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
   getOn(callback: CharacteristicGetCallback) {
-    // implement your own code to check if the device is on
     const isOn = this.doorStates.On;
 
     this.platform.log.debug('Get Characteristic On ->', isOn);
 
-    // you must call the callback function
-    // the first argument should be null if there were no errors
-    // the second argument should be the value to return
     callback(null, isOn);
   }
 }
